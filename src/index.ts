02d379d1000e7f37e0a663f230bef4d40b7c4de2
iury <correiaiury10@gmail.com>
2025-09-11 15:13:41 -0300

    Aula 1

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+import login from"./controllers/login";
+
+app.use('/', login)
+
+app.listen(process.env.PORT, () => {
+    console.log( `Servidor Iniciado na porta ${process.env.PORT}: http://localhost:${process.env.PORT}`);
+});
